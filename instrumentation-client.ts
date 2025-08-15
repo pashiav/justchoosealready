b83914a14cb93a27b189e8b91a2a8565f3c30e29
pashiav <pashiavang2022@gmail.com>
2025-08-15 13:12:12 -0500

    fix: posthog api_host to us link

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,9 +1,8 @@
 import posthog from "posthog-js"
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-  api_host: "/ingest",
+  api_host: "https://us.posthog.com", // or https://eu.posthog.com if EU project
   ui_host: "https://us.posthog.com",
-  defaults: '2025-05-24',
   capture_exceptions: true,
   debug: process.env.NODE_ENV === "development",
 });
